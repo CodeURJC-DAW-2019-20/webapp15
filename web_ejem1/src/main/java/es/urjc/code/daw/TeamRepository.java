595c333c40ba69ee523fac6dd502339fdedc4fe1
juan <je.anton@alumnos.urjc.es>
2020-02-20 15:05:55 +0100

    Project without error (2)

---
+++ web_ejem1/src/main/java/es/urjc/code/daw/TeamRepository.java
@@ -0,0 +1,13 @@
+package es.urjc.code.daw;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team,Long> {
+	public Optional<Team> findById(Long id);
+	public Optional<Team> findByName(String name);	
+}
