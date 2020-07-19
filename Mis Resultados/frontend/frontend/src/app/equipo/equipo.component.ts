c5c78adacbba272679ed95d22e62d92cc295b70d
juanAntonMarazuela <32297547+juanAntonMarazuela@users.noreply.github.com>
2020-07-19 23:55:31 +0200

    Register user favorite

--- Mis Resultados/frontend/frontend/src/app/equipo/equipo.component.ts
+++ Mis Resultados/frontend/frontend/src/app/equipo/equipo.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient,HttpHeaders} from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Team } from 'src/app/team';
+import { LocalStorage } from 'ngx-webstorage';
+import { LoginComponent } from '../login/login.component';
+import { LoginService } from '../login.service';
 
 
 @Component({
@@ -18,7 +21,12 @@ export class EquipoComponent implements OnInit {
   name: string;
 
 
-  constructor(private http: HttpClient,private router: Router, activatedRoute: ActivatedRoute) {
+  @LocalStorage('currentUser')
+  public currentUser;
+  @LocalStorage('isUserLogged')
+  public isUserLoggedIn;
+
+  constructor(private http: HttpClient,private router: Router, activatedRoute: ActivatedRoute,loginService: LoginService) {
     let id = activatedRoute.snapshot.params['id'];
     let url = environment.apiEndPoint + '/equipos/'+id;
     this.http.get<Team>(url).subscribe(
@@ -38,7 +46,19 @@ export class EquipoComponent implements OnInit {
   getTeamData(id : any, url: string){
     
     return this.http.get<Team>(url).pipe(map(team => team));
-
+  }
+  updateFav(teamName:string){
+    let url = environment.apiEndPoint + '/user/updateFav/'+this.currentUser.name+'/'+teamName;
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    this.http.put<Team>(url,headers).subscribe(
+      response => {
+      },
+      error =>{
+        console.error(error);
+      }
+    )
   }
 
 }
